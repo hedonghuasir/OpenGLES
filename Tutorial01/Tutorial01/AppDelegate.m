0ee0e22f7cf4cb0f1c2884e6e913a511caa69f72
hedonghua <hedonghua@hoolai.com>
2019-04-24 17:43:36 +0800

    解决编译问题

--- Tutorial01/Tutorial01/AppDelegate.m
+++ Tutorial01/Tutorial01/AppDelegate.m
@@ -23,6 +23,22 @@
     
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
+    //解决该问题的代码
+    NSArray *windows = [[UIApplication sharedApplication] windows];
+    for(UIWindow *window in windows) {
+        if(window.rootViewController == nil){
+            UIViewController *vc = [[UIViewController alloc]initWithNibName:nil
+                                                                     bundle:nil];
+            window.rootViewController = vc;
+        }
+    }
+    //解决该问题的代码
+    //----------------------------------------------------------------------------------------
+
+//    UIViewController*emptyView=[[UIViewControlleralloc]initNibName:nilbundle:nil];
+//    self.window.rootViewController=emptyView;
+    
+
     return YES;
 }
 
